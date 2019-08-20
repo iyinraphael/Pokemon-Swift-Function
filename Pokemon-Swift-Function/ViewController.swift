dbe274432b49ef92ad7cda2ad09d1df1f536dabc
iyinraphael <iyinraphael@gmail.com>
2019-08-20 00:59:39 -0700

    intial set up

---
+++ Pokemon-Swift-Function/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemon-Swift-Function
+//
+//  Created by Iyin Raphael on 8/20/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
