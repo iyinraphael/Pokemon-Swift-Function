81090278feb95dd8b4c674f056b466ceebba844d
iyinraphael <iyinraphael@gmail.com>
2019-08-20 01:40:36 -0700

    A simple network call on twilio function

--- Pokemon-Swift-Function/ViewController.swift
+++ Pokemon-Swift-Function/ViewController.swift
@@ -11,6 +11,28 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func buttonClicked(_ sender: Any) {
+        
+        let functionURL = "https://denim-armadillo-3334.twil.io/swift"
+        if let url = URL(string: functionURL) {
+            let task = URLSession.shared.dataTask(with: url) {
+                data, response, error in
+                if error != nil {
+                    print(error!)
+                } else {
+                    do {
+                        let responseObject = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [[String: Any]]
+                        DispatchQueue.main.async {
+                            self.jokeLabel.text = (responseObject![0]["text"] as! String)
+                            print("\(responseObject)")
+                            self.jokeLabel.font = self.jokeLabel.font.withSize(12)
+                        }
+                    } catch let error as NSError {
+                        print(error)
+                    }
+                }
+            }
+            task.resume()
+        }
     }
     
     @IBOutlet weak var jokeLabel: UILabel!
